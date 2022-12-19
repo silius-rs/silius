ac02fef3f6c25233ee65ac267296b53997ad9fc9
Will Qiu <zsluedem06@gmail.com>
2022-12-19 12:03:21 +0800

    add entry point api

---
+++ src/contracts/gen.rs
@@ -0,0 +1,47 @@
+use crate::types::user_operation::UserOperation as UserOp;
+use ethers::contract::abigen;
+abigen!(
+    AggregatedAccount,
+    "$OUT_DIR/IAggregatedAccount.sol/IAggregatedAccount.json"
+);
+
+abigen!(Aggregator, "$OUT_DIR/IAggregator.sol/IAggregator.json");
+
+abigen!(
+    Create2Deployer,
+    "$OUT_DIR/ICreate2Deployer.sol/ICreate2Deployer.json"
+);
+
+abigen!(EntryPointAPI, "$OUT_DIR/IEntryPoint.sol/IEntryPoint.json");
+
+abigen!(Paymaster, "$OUT_DIR/IPaymaster.sol/IPaymaster.json");
+
+abigen!(
+    StakeManager,
+    "$OUT_DIR/IStakeManager.sol/IStakeManager.json"
+);
+
+abigen!(Account, "$OUT_DIR/IAccount.sol/IAccount.json");
+
+abigen!(
+    UserOperation,
+    "$OUT_DIR/UserOperation.sol/UserOperationLib.json"
+);
+
+impl From<UserOp> for entry_point_api::UserOperation {
+    fn from(value: UserOp) -> Self {
+        Self {
+            sender: value.sender,
+            nonce: value.nonce,
+            init_code: value.init_code,
+            call_data: value.call_data,
+            call_gas_limit: value.call_gas_limit,
+            verification_gas_limit: value.verification_gas_limit,
+            pre_verification_gas: value.pre_verification_gas,
+            max_fee_per_gas: value.max_fee_per_gas,
+            max_priority_fee_per_gas: value.max_priority_fee_per_gas,
+            paymaster_and_data: value.paymaster_and_data,
+            signature: value.signature,
+        }
+    }
+}
--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -1,29 +1,4 @@
-use ethers::contract::abigen;
+mod entrypoint;
+mod gen;
 
-abigen!(
-    AggregatedAccount,
-    "$OUT_DIR/IAggregatedAccount.sol/IAggregatedAccount.json"
-);
-
-abigen!(Aggregator, "$OUT_DIR/IAggregator.sol/IAggregator.json");
-
-abigen!(
-    Create2Deployer,
-    "$OUT_DIR/ICreate2Deployer.sol/ICreate2Deployer.json"
-);
-
-abigen!(EntryPoint, "$OUT_DIR/IEntryPoint.sol/IEntryPoint.json");
-
-abigen!(Paymaster, "$OUT_DIR/IPaymaster.sol/IPaymaster.json");
-
-abigen!(
-    StakeManager,
-    "$OUT_DIR/IStakeManager.sol/IStakeManager.json"
-);
-
-abigen!(Account, "$OUT_DIR/IAccount.sol/IAccount.json");
-
-abigen!(
-    UserOperation,
-    "$OUT_DIR/UserOperation.sol/UserOperationLib.json"
-);
+pub use entrypoint::*;
