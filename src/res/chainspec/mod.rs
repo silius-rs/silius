97cedfbde5e88414b2e7f2f8d2582af399803ba2
Will Qiu <zsluedem06@gmail.com>
2023-03-27 11:17:10 +0800

    chore: remove unused res folder

--- src/res/chainspec/mod.rs
+++
@@ -1,7 +0,0 @@
-use crate::models::ChainSpec;
-use once_cell::sync::Lazy;
-
-pub static MAINNET: Lazy<ChainSpec> =
-    Lazy::new(|| ron::from_str(include_str!("ethereum.ron")).unwrap());
-pub static GOERLI: Lazy<ChainSpec> =
-    Lazy::new(|| ron::from_str(include_str!("goerli.ron")).unwrap());
