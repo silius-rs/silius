19f5522342d8fc20fa5682e85cd28629c858d6b1
harry <gaohaoyuandd@gmail.com>
2023-07-25 02:15:26 -0400

    fixed create-wallet

--- bin/bundler/src/create-wallet.rs
+++ bin/bundler/src/create-wallet.rs
@@ -1,3 +1,5 @@
+use std::fs::create_dir_all;
+
 use aa_bundler::utils::parse_u256;
 use aa_bundler_primitives::Wallet;
 use anyhow::Result;
@@ -31,29 +33,30 @@ fn main() -> Result<()> {
     let path = if let Some(output_path) = opt.output_path {
         output_path
     } else {
-        home_dir()
-            .map(|h| h.join(".aa-bundler/bundler.json"))
-            .ok_or_else(|| anyhow::anyhow!("Get Home directory error"))
-            .map(ExpandedPathBuf)?
+        let default_path = home_dir()
+            .map(|h| h.join(".aa-bundler/bundler"))
+            .ok_or_else(|| anyhow::anyhow!("Get Home directory error"))?;
+        create_dir_all(&default_path)?;
+        ExpandedPathBuf::from(default_path)
     };
 
     let fb_path = if let Some(fb_bundler_path) = opt.fb_bundler_path.clone() {
         fb_bundler_path
     } else {
-        home_dir()
-            .map(|h| h.join(".aa-bundler/fb-bundler.json"))
-            .ok_or_else(|| anyhow::anyhow!("Get Home directory error"))
-            .map(ExpandedPathBuf)?
+        let default_path = home_dir()
+            .map(|h| h.join(".aa-bundler/fb-bundler"))
+            .ok_or_else(|| anyhow::anyhow!("Get Home directory error"))?;
+        create_dir_all(&default_path)?;
+        ExpandedPathBuf::from(default_path)
     };
 
     if opt.fb_bundler_path.is_some() {
-        let wallet = Wallet::build_random(path.clone(), &opt.chain_id, None)?;
-        info!("Wallet's Signer {:?}", wallet.signer);
-        let fb_wallet = Wallet::build_random(path, &opt.chain_id, Some(fb_path))?;
-        info!("Flashbots' Bundle Signer {:?}", fb_wallet.signer);
-    } else {
         let wallet = Wallet::build_random(path, &opt.chain_id, None)?;
-        info!("Wallet's Signer {:?}", wallet.signer);
+        info!("Wallet Signer {:?}", wallet.signer);
+    } else {
+        let fb_wallet = Wallet::build_random(path, &opt.chain_id, Some(fb_path))?;
+        info!("Wallet Signer {:?}", fb_wallet.signer);
+        info!("Flashbots Signer {:?}", fb_wallet.fb_signer);
     }
 
     Ok(())
