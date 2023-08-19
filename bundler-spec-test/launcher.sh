ae90235158e1f99cb0cc5b699b06af0b61dee4c6
katsumata <12413150+winor30@users.noreply.github.com>
2023-08-19 19:53:23 +0900

    feat: Update bundler-spec-test launcher with additional options
    - Added options `--http` and `--ws` to the `silius` command in `launcher.sh` file
    - Added `--rpc-api` option with the values `eth,debug,web3` to the `silius` command in `launcher.sh` file

--- bundler-spec-test/launcher.sh
+++ bundler-spec-test/launcher.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 # Launcher script for the Silius.
-set -x 
+set -x
 pushd `dirname \`realpath $0\``
 case $1 in
 
@@ -16,6 +16,8 @@ case $1 in
         --mnemonic-file keys/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
         --beneficiary 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990 \
         --entry-points 0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789 \
+        --http \
+        --ws \
         --rpc-api eth,debug,web3 & echo $! > bundler.pid
     popd
 	cd @account-abstraction && yarn deploy --network localhost
