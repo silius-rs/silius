a6159bfc82c3f5754ccff832affd14602d82add2
Vid Kersic <vid.kersic@yahoo.com>
2023-08-28 16:02:14 +0200

    chore: update js tracer, rename bundler spec tests folder

--- crates/contracts/src/tracer.rs
+++ crates/contracts/src/tracer.rs
@@ -99,9 +99,11 @@ pub const JS_TRACER: &str = r#"
     stopCollectingTopic: 'bb47ee3e183a558b1a2ff0874b079f3fc5478b7454eacf2bfc5af2ff5878f972',
     stopCollecting: false,
     topLevelCallCounter: 0,
+
     fault: function(log, db) {
         this.debug.push(`fault depth=${log.getDepth()} gas=${log.getGas()} cost=${log.getCost()} err=${log.getError()}`);
     },
+
     result: function(ctx, db) {
         return {
             callsFromEntryPoint: this.callsFromEntryPoint,
@@ -111,6 +113,7 @@ pub const JS_TRACER: &str = r#"
             debug: this.debug // for internal debugging.
         };
     },
+
     enter: function(frame) {
         if (this.stopCollecting) {
             return;
@@ -125,6 +128,7 @@ pub const JS_TRACER: &str = r#"
             value: frame.getValue()
         });
     },
+
     exit: function(frame) {
         if (this.stopCollecting) {
             return;
@@ -135,20 +139,22 @@ pub const JS_TRACER: &str = r#"
             data: toHex(frame.getOutput()).slice(0, 4000)
         });
     },
+
     // increment the "key" in the list. if the key is not defined yet, then set it to "1"
     countSlot: function(list, key) {
         var _a;
         list[key] = ((_a = list[key]) !== null && _a !== void 0 ? _a : 0) + 1;
     },
+
     step: function(log, db) {
         var _a;
         if (this.stopCollecting) {
             return;
         }
-        var opcode = log.op.toString();
-        var stackSize = log.stack.length();
-        var stackTop3 = [];
-        for (var i = 0; i < 3 && i < stackSize; i++) {
+        const opcode = log.op.toString();
+        const stackSize = log.stack.length();
+        const stackTop3 = [];
+        for (let i = 0; i < 3 && i < stackSize; i++) {
             stackTop3.push(log.stack.peek(i));
         }
         this.lastThreeOpcodes.push({
@@ -166,9 +172,9 @@ pub const JS_TRACER: &str = r#"
             if (log.getDepth() === 1) {
                 // exit() is not called on top-level return/revent, so we reconstruct it
                 // from opcode
-                var ofs = parseInt(log.stack.peek(0).toString());
-                var len = parseInt(log.stack.peek(1).toString());
-                var data = toHex(log.memory.slice(ofs, ofs + len)).slice(0, 4000);
+                const ofs = parseInt(log.stack.peek(0).toString());
+                const len = parseInt(log.stack.peek(1).toString());
+                const data = toHex(log.memory.slice(ofs, ofs + len)).slice(0, 4000);
                 // this.debug.push(opcode + ' ' + data)
                 this.calls.push({
                     type: opcode,
@@ -182,12 +188,12 @@ pub const JS_TRACER: &str = r#"
         if (log.getDepth() === 1) {
             if (opcode === 'CALL' || opcode === 'STATICCALL') {
                 // stack.peek(0) - gas
-                var addr = toAddress(log.stack.peek(1).toString(16));
-                var topLevelTargetAddress = toHex(addr);
+                const addr = toAddress(log.stack.peek(1).toString(16));
+                const topLevelTargetAddress = toHex(addr);
                 // stack.peek(2) - value
-                var ofs = parseInt(log.stack.peek(3).toString());
+                const ofs = parseInt(log.stack.peek(3).toString());
                 // stack.peek(4) - len
-                var topLevelMethodSig = toHex(log.memory.slice(ofs, ofs + 4));
+                const topLevelMethodSig = toHex(log.memory.slice(ofs, ofs + 4));
                 this.currentLevel = this.callsFromEntryPoint[this.topLevelCallCounter] = {
                     topLevelMethodSig: topLevelMethodSig,
                     topLevelTargetAddress: topLevelTargetAddress,
@@ -199,7 +205,7 @@ pub const JS_TRACER: &str = r#"
                 this.topLevelCallCounter++;
             } else if (opcode === 'LOG1') {
                 // ignore log data ofs, len
-                var topic = log.stack.peek(2).toString(16);
+                const topic = log.stack.peek(2).toString(16);
                 if (topic === this.stopCollectingTopic) {
                     this.stopCollecting = true;
                 }
@@ -207,12 +213,12 @@ pub const JS_TRACER: &str = r#"
             this.lastOp = '';
             return;
         }
-        var lastOpInfo = this.lastThreeOpcodes[this.lastThreeOpcodes.length - 2];
+        const lastOpInfo = this.lastThreeOpcodes[this.lastThreeOpcodes.length - 2];
         // store all addresses touched by EXTCODE* opcodes
         if (((_a = lastOpInfo === null || lastOpInfo === void 0 ? void 0 : lastOpInfo.opcode) === null || _a === void 0 ? void 0 : _a.match(/^(EXT.*)$/)) != null) {
-            var addr = toAddress(lastOpInfo.stackTop3[0].toString(16));
-            var addrHex = toHex(addr);
-            var last3opcodesString = this.lastThreeOpcodes.map(function(x) {
+            const addr = toAddress(lastOpInfo.stackTop3[0].toString(16));
+            const addrHex = toHex(addr);
+            const last3opcodesString = this.lastThreeOpcodes.map(function(x) {
                 return x.opcode;
             }).join(' ');
             // only store the last EXTCODE* opcode per address - could even be a boolean for our current use-case
@@ -224,16 +230,16 @@ pub const JS_TRACER: &str = r#"
             }
         }
         // not using 'isPrecompiled' to only allow the ones defined by the ERC-4337 as stateless precompiles
-        var isAllowedPrecompiled = function(address) {
-            var addrHex = toHex(address);
-            var addressInt = parseInt(addrHex);
+        const isAllowedPrecompiled = function(address) {
+            const addrHex = toHex(address);
+            const addressInt = parseInt(addrHex);
             // this.debug.push(`isPrecompiled address=${addrHex} addressInt=${addressInt}`)
             return addressInt > 0 && addressInt < 10;
         };
         if (opcode.match(/^(EXT.*|CALL|CALLCODE|DELEGATECALL|STATICCALL)$/) != null) {
-            var idx = opcode.startsWith('EXT') ? 0 : 1;
-            var addr = toAddress(log.stack.peek(idx).toString(16));
-            var addrHex = toHex(addr);
+            const idx = opcode.startsWith('EXT') ? 0 : 1;
+            const addr = toAddress(log.stack.peek(idx).toString(16));
+            const addrHex = toHex(addr);
             // this.debug.push('op=' + opcode + ' last=' + this.lastOp + ' stacksize=' + log.stack.length() + ' addr=' + addrHex)
             if (this.currentLevel.contractSize[addrHex] == null && !isAllowedPrecompiled(addr)) {
                 this.currentLevel.contractSize[addrHex] = {
@@ -254,11 +260,11 @@ pub const JS_TRACER: &str = r#"
         }
         this.lastOp = opcode;
         if (opcode === 'SLOAD' || opcode === 'SSTORE') {
-            var slot = toWord(log.stack.peek(0).toString(16));
-            var slotHex = toHex(slot);
-            var addr = log.contract.getAddress();
-            var addrHex = toHex(addr);
-            var access = this.currentLevel.access[addrHex];
+            const slot = toWord(log.stack.peek(0).toString(16));
+            const slotHex = toHex(slot);
+            const addr = log.contract.getAddress();
+            const addrHex = toHex(addr);
+            let access = this.currentLevel.access[addrHex];
             if (access == null) {
                 access = {
                     reads: {},
@@ -278,8 +284,8 @@ pub const JS_TRACER: &str = r#"
         }
         if (opcode === 'KECCAK256') {
             // collect keccak on 64-byte blocks
-            var ofs = parseInt(log.stack.peek(0).toString());
-            var len = parseInt(log.stack.peek(1).toString());
+            const ofs = parseInt(log.stack.peek(0).toString());
+            const len = parseInt(log.stack.peek(1).toString());
             // currently, solidity uses only 2-word (6-byte) for a key. this might change..
             // still, no need to return too much
             if (len > 20 && len < 512) {
@@ -287,15 +293,15 @@ pub const JS_TRACER: &str = r#"
                 this.keccak.push(toHex(log.memory.slice(ofs, ofs + len)));
             }
         } else if (opcode.startsWith('LOG')) {
-            var count = parseInt(opcode.substring(3));
-            var ofs = parseInt(log.stack.peek(0).toString());
-            var len = parseInt(log.stack.peek(1).toString());
-            var topics = [];
-            for (var i = 0; i < count; i++) {
+            const count = parseInt(opcode.substring(3));
+            const ofs = parseInt(log.stack.peek(0).toString());
+            const len = parseInt(log.stack.peek(1).toString());
+            const topics = [];
+            for (let i = 0; i < count; i++) {
                 // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
                 topics.push('0x' + log.stack.peek(2 + i).toString(16));
             }
-            var data = toHex(log.memory.slice(ofs, ofs + len));
+            const data = toHex(log.memory.slice(ofs, ofs + len));
             this.logs.push({
                 topics: topics,
                 data: data
