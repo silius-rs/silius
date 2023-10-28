90e96dbd166b321238eea4aeb7d112f8c1088e3b
Vid Kersic <vid.kersic@yahoo.com>
2023-10-28 15:45:24 +0200

    fix: ci

--- bundler-spec-tests/launcher.sh
+++ bundler-spec-tests/launcher.sh
@@ -10,10 +10,9 @@ case $1 in
 
  start)
 	docker-compose up -d
-    sleep 2
     silius bundler \
         --verbosity 4 \
-        --eth-client-address ws://127.0.0.1:8546 \
+        --eth-client-address http://127.0.0.1:8545 \
         --mnemonic-file keys/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
         --beneficiary 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
         --entry-points 0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789 \
@@ -26,11 +25,12 @@ case $1 in
         --ws.port 3001 \
         --ws.api eth,debug,web3 & echo $! > bundler.pid
     popd
-	cd @account-abstraction && yarn deploy --network localhost
+    cd @account-abstraction && yarn deploy --network localhost
 	;;
  stop)
  	docker-compose down
     kill $(cat bundler.pid)
+    rm bundler.pid
 	;;
 
  *)
