746056164455ba5841f1af58387f806d85ea47da
Vid Kersic <vid.kersic@yahoo.com>
2023-12-30 11:49:15 +0100

    chore: update docker build, ci

---
+++ .github/scripts/install_solc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# install solc
+
+SOLC_VERSION="v0.8.20"
+
+wget -c "https://github.com/ethereum/solidity/releases/download/$SOLC_VERSION/solc-static-linux"
+mv solc-static-linux /usr/local/bin/solc
+chmod a+x /usr/local/bin/solc
+
+solc --version
