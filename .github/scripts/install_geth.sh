7f69b4fb55e7dd41ec4a21794de8862805b335d3
Vid Kersic <vid.kersic@yahoo.com>
2024-09-20 23:44:04 +0200

    chore: update geth

--- .github/scripts/install_geth.sh
+++ .github/scripts/install_geth.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 # install geth
 
-GETH_VERSION="1.12.0-e501b3b0"
+GETH_VERSION="1.14.8-a9523b64"
 
 wget -c "https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-$GETH_VERSION.tar.gz"
 tar -xf "geth-linux-amd64-$GETH_VERSION.tar.gz"
